06eabdad6a60144c3e39f7c4511bf42be45caf97
sanjit sahoo <sanjit sahoo@LAPTOP-BNLU8FNL>
2020-10-20 12:42:25 +0530

    commiting to members regards

---
+++ src/test/java/restapi/Weather_GET_Request.java
@@ -0,0 +1,12 @@
+package restapi;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class Weather_GET_Request {
+
+	@Test
+	void verifyTest() {
+		Assert.assertTrue(true);
+	}
+}
